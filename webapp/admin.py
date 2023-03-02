a826dd602a36db62fb30db150deec4c334ed349b
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-03 02:32:34 +0600

    Модели зарегистрированы все в админ-панели

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from webapp.models.statuses import Status
+from webapp.models.tasks import Task
+from webapp.models.types import Type
+
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(Type)
+admin.site.register(Status)
