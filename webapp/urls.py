69815bebd4c4a668ee2041e22006fd39958e38a8
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-03 18:57:33 +0600

    Изменения в файлах

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from webapp.views import TaskAddView, TaskDetailView, TaskUpdateView, TaskDeleteView, ConfirmDeleteView, IndexView, \
+    IndexRedirectView
 
 urlpatterns = [
-    # path('', views.index_view),
-
+    path("", IndexView.as_view(), name='index'),
+    path("task/", IndexRedirectView.as_view(), name='task_index_redirect'),
+    path("task/add", TaskAddView.as_view(), name='task_add'),
+    path('task/<int:pk>', TaskDetailView.as_view(), name='task_detail'),
+    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+    path('task/<int:pk>/confirm_delete/', ConfirmDeleteView.as_view(), name='confirm_delete'),
 ]
