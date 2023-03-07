ec18e13b41b356c057e8f001749b4f0339e5cd2d
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-07 10:47:53 +0600

    Добавлены валидаторв

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,10 +1,35 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, BaseValidator
 
 from webapp.models import Task
 
 
+def max_len_validator(string):
+    if len(string) < 100:
+        raise ValidationError('Описание не должно быть короче 100 символов!')
+    return string
+
+
+class CustomLenValidator(BaseValidator):
+    def __init__(self, limit_value=20):
+        message = 'Максимальная длина заголовка %(limit_value)s. Вы ввели %(show_value)s символов'
+        super().__init__(limit_value=limit_value, message=message)
+
+    def compare(self, value, limit_value):
+        return value > limit_value
+
+    def clean(self, value):
+        return len(value)
+
+
 class TaskForm(forms.ModelForm):
+    summary = forms.CharField(max_length=100, validators=(MinLengthValidator(limit_value=3, message='Заголовок должен '
+                                                                                                    'быть длиннее 2 '
+                                                                                                    'символов!'),
+                                                          CustomLenValidator()))
+    description = forms.CharField(validators=(max_len_validator,))
+
     class Meta:
         model = Task
         fields = ('summary', 'description', 'status', 'type')
@@ -15,9 +40,8 @@ class TaskForm(forms.ModelForm):
             'type': 'Тип задачи'
         }
 
-        def clean_title(self):
-            summary = self.cleaned_data.get('summary')
-            if len(summary) < 2:
-                raise ValidationError('Заголовок должен быть длиннее 2 символов')
-            return summary
-
+    def clean_summary(self):
+        summary = self.cleaned_data.get('summary')
+        if Task.objects.filter(summary=summary).exists():
+            raise ValidationError('Такой заголовок уже есть!')
+        return summary
