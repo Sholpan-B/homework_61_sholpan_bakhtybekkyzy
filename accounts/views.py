4fe6087667a39317320ad1c294002d9c3bccb557
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-17 02:53:24 +0600

    добавлены представления login_view и logout_view

---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+
+def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            context['has_error'] = True
+    return render(request, 'login.html', context=context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
