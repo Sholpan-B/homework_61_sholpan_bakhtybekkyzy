e2b3389c993962e442cc4d95a99cc6b8f61602c9
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-17 02:58:55 +0600

    настройки в settings.py

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-    'webapp'
+    'webapp',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
